cd8c1b3b65ef2ebc58fe89e4b12cfea2ecf8487b
Hiep Nguyen <hiepnh610@gmail.com>
2019-09-03 22:59:44 +0700

    add form control for sign up page

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+
+  public frm = new FormGroup({
+    email: new FormControl(''),
+    username: new FormControl(''),
+    fullName: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
 
   constructor() { }
 
