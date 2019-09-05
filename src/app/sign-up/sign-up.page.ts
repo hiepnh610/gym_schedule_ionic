450eb423cbd3e50c26c25e52d8a0d543bcd8f432
Hiep Nguyen <hiepnh610@gmail.com>
2019-09-05 19:56:41 +0700

    init form for sign up

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,12 +9,48 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class SignUpPage implements OnInit {
 
   public frm = new FormGroup({
-    email: new FormControl(''),
-    username: new FormControl(''),
-    fullName: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl('')
+    email: new FormControl('', [
+      Validators.required
+    ]),
+    username: new FormControl('', [
+      Validators.required
+    ]),
+    fullName: new FormControl('', [
+      Validators.required
+    ]),
+    password: new FormControl('', [
+      Validators.required
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required
+    ])
   });
+
+  public formErrors = {
+    email: '',
+    username: '',
+    fullName: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  public validationMessages = {
+    email: {
+      required: 'Email is required.'
+    },
+    username: {
+      required: 'Username is required.'
+    },
+    fullName: {
+      required: 'Full name is required.'
+    },
+    password: {
+      required: 'Password is required.'
+    },
+    confirmPassword: {
+      required: 'Confirm password is required.'
+    }
+  };
 
   constructor() { }
 
