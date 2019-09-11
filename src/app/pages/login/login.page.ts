935f47a9ec2f9da5aecab228b591b408ac743be0
Hiep Nguyen <hiepnh610@gmail.com>
2019-09-11 08:09:33 +0700

    handle form for login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { BaseComponent } from '@common/base/base.component';
+import { ERROR_MESSAGES } from '@constants/messages';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage extends BaseComponent implements OnInit {
 
-  constructor() { }
+  public frm: FormGroup;
+
+  public controlConfig = {
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  };
+
+  public formErrors = {
+    username: '',
+    password: ''
+  };
+
+  public validationMessages = {
+    username: {
+      required: ERROR_MESSAGES.USERNAME_REQUIRED,
+      minlength: ERROR_MESSAGES.MIN_LENGTH
+    },
+    password: {
+      required: ERROR_MESSAGES.PASSWORD_REQUIRED,
+      minlength: ERROR_MESSAGES.MIN_LENGTH
+    }
+  };
+
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
+    super.ngOnInit();
   }
+
+  get username() { return this.frm.get('username'); }
+
+  get password() { return this.frm.get('password'); }
 
 }
